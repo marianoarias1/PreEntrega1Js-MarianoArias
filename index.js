5df5e48a2cabfd33714d9886f1ec23f8b930f658
marianoarias1 <marianoarias123@hotmail.com>
2023-08-09 11:11:32 -0300

    agregado de funcion

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const nombreCliente= prompt("Ingrese su Nombre, por favor")
 
-const edadCliente= prompt("Ahora ingrese su edad")
+const edadCliente= parseInt(prompt("Ahora ingrese su edad"))
 
-while(edadCliente >= 50){
-    const precioDeCompra= parseInt(prompt("ingrese el total de su compra"))
-    const descuentoMayores= precioDeCompra * 30 / 100
-    const PrecioFinal= precioDeCompra - descuentoMayores
-    alert(`${nombreCliente}, el precio final despues de su descuento es de $${PrecioFinal}, que lo disfrute`)
-
-    break
+const funcionDescuento=(nombre, edad)=>{
+    while(edad >= 50){
+        const precioDeCompra= parseInt(prompt("ingrese el total de su compra"))
+        const descuentoMayores= precioDeCompra * 30 / 100
+        const PrecioFinal= precioDeCompra - descuentoMayores
+        alert(`${nombre}, el precio final despues de su descuento es de $${PrecioFinal}, que lo disfrute`)
+    
+        break
+    }
+    if(edad < 50){
+        alert(`${nombre} lo lamentamos, no cumples la edad de requisito para el descuento :c`)
+    }
 }
 
-if(edadCliente < 50){
-    alert(`${nombreCliente} lo lamentamos, no cumples la edad de requisito para el descuento :c`)
-}
+funcionDescuento(nombreCliente, edadCliente)
+
+
+
